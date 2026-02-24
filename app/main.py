08adc77415ec9dbf71961f967827ad1c0be655ca
Rostislick <yaretsro@mail.ru>
2026-02-24 23:45:27 +0700

    add Alembic

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 "main"
 from fastapi import FastAPI
-from app.database import engine, Base
 from app.models import User, Movie, UserMovie 
-
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Movie Tracker")
 
